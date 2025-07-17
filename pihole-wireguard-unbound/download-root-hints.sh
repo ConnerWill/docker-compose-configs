3ef6570f5dc690fe932f0b9711a0f731b0553215
ConnerWill <conner.will@outlook.com>
2025-07-17 15:17:45 -0500

    Removed root hints

--- pihole-wireguard-unbound/download-root-hints.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-ENV_FILE="./.env"
-ROOT_HINTS_URL="https://www.internic.net/domain/named.cache"
-
-source "${ENV_FILE}" || return 1
-
-curl -o "${UNBOUND_CONFIG_DIR}/root.hints" "${ROOT_HINTS_URL}"
