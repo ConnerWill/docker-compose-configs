97015f24efaa1c0ab942abb887064e4d878ae6d2
ConnerWill <conner.will@outlook.com>
2025-07-17 14:35:08 -0500

    Added scripts to be dynamic

--- pihole-wireguard/docker-compose-kill-all.sh
+++ pihole-wireguard/docker-compose-kill-all.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+ENV_FILE="./.env"
+PIHOLE_NETWORK_NAME="pihole_vpn"
+
+source "${ENV_FILE}" || return 1
+
 docker-compose kill
-docker container rm pihole-pihole pihole-wireguard
-docker network rm pihole_vpn
-docker image rm lscr.io/linuxserver/wireguard:latest
-docker image rm pihole/pihole:latest
+docker container rm ${PIHOLE_CONTAINER_NAME} ${WIREGUARD_CONTAINER_NAME}
+docker network rm "${PIHOLE_NETWORK_NAME}"
+docker image rm "${WIREGUARD_IMAGE_NAME}"
+docker image rm "${PIHOLE_IMAGE_NAME}"
