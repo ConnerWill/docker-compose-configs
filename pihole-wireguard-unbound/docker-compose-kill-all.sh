876faf1141b9abfea42f2c2b4ac3c68066cb2bb8
ConnerWill <conner.will@outlook.com>
2025-07-17 14:56:12 -0500

    Added unbound

---
+++ pihole-wireguard-unbound/docker-compose-kill-all.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+ENV_FILE="./.env"
+PIHOLE_NETWORK_NAME="pihole_vpn"
+
+source "${ENV_FILE}" || return 1
+
+docker-compose kill
+docker container rm ${PIHOLE_CONTAINER_NAME} ${WIREGUARD_CONTAINER_NAME}
+docker network rm "${PIHOLE_NETWORK_NAME}"
+docker image rm "${WIREGUARD_IMAGE_NAME}"
+docker image rm "${PIHOLE_IMAGE_NAME}"
