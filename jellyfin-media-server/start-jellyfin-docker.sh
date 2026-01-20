efec09293d7b60aa796fbcc73dc10e1fdcd72a99
ConnerWill <conner.will@outlook.com>
2026-01-20 11:13:34 -0600

    Updated script

--- jellyfin-media-server/start-jellyfin-docker.sh
+++ jellyfin-media-server/start-jellyfin-docker.sh
@@ -9,6 +9,7 @@ set -e
 SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}")"
 SCRIPT_DIR="$(dirname "${SCRIPT_PATH}")"
 DOCKER_COMPOSE_FILE="${SCRIPT_DIR}/docker-compose.yml"
+ENV_FILE="${SCRIPT_DIR}/.env"
 TEXT_GREEN='\x1B[38;5;46m'
 TEXT_RED='\x1B[38;5;196m'
 TEXT_RESET='\x1B[0m'
@@ -36,6 +37,16 @@ function file_exists(){
   fi
 }
 
+function source_file(){
+  local input_file
+  input_file="${1}"
+  if [[ -e "${input_file}" ]]; then
+    if source "${input_file}"; then
+      die "Unable to source file: '${input_file}'"
+    fi
+  fi
+}
+
 function is_installed(){
   local input_pkg
   input_pkg="${1}"
@@ -45,12 +56,18 @@ function is_installed(){
 }
 
 function start_docker(){
-  docker-compose up --wait --detach
+  if docker-compose --file "${DOCKER_COMPOSE_FILE}" --env-file "${ENV_FILE}" up --wait --detach; then
+    success "Started docker-compose: ${JELLYFIN_NAME}"
+  else
+    die "Failed to start docker-compose"
+  fi
 }
 
 ## MAIN
 
 file_exists "${DOCKER_COMPOSE_FILE}"
+file_exists "${ENV_FILE}"
+source_file "${ENV_FILE}"
 is_installed "docker"
 is_installed "docker-compose"
 start_docker
