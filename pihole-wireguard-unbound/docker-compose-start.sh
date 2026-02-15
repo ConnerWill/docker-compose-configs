dbafafdcf428f0d9c85bba885a5bcfc9c6f2f30e
ConnerWill <conner.will@outlook.com>
2026-02-15 03:42:05 +0000

    Made script better

--- pihole-wireguard-unbound/docker-compose-start.sh
+++ pihole-wireguard-unbound/docker-compose-start.sh
@@ -1,3 +1,32 @@
 #!/usr/bin/env bash
 
-docker-compose up --pull always --yes --detach --wait
+set -e
+
+declare -a compose_args
+compose_args=(
+  up
+  --force-recreate
+  --pull always
+  --yes
+  --detach
+)
+
+is_installed(){
+  local pkg found_pkg
+  pkg="${1}"
+  if command -v "${pkg}" >/dev/null 2>&1; then
+    found_pkg="$(command -v ${pkg} || true)"
+    echo "${found_pkg}"
+  else
+    printf "Cannot find '%s' in PATH\n" >2
+    return 1
+  fi
+}
+
+# TODO: Fix this. make better arg passing
+if [[ "${1}" == "-d" || "${1}" == "--dry-drun" || "${1}" == "--dryrun" || "${1}" == "--dry" ]]; then
+  compose_args+=( --dry-run )
+fi
+
+compose_bin="$(is_installed 'docker-compose')"
+"${compose_bin}" "${compose_args[@]}"
